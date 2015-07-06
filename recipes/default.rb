a7269f13d0368a4aac101340278033aa687e7997
Bryan Bishop <bryan.bishop@pearson.com>
2015-07-06 17:27:15 -0500

    parens in the wrong place

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,13 +10,13 @@ file "raise the ulimit for duplicity" do
   mode "0700"
 end
 
-directory ::File.join(::Dir.home(node['current_user'], ".duplicity")) do
+directory ::File.join(::Dir.home(node['current_user']), ".duplicity") do
   owner node['current_user']
   group node['current_user']
   mode "0750"
 end
 
-directory ::File.join(::Dir.home(node['current_user'], ".duply")) do
+directory ::File.join(::Dir.home(node['current_user']), ".duply") do
   owner node['current_user']
   group node['current_user']
   mode "0750"
