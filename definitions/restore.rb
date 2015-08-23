653fee591862c28ac32a65efda2d2a18f44756fe
Bryan Bishop <bryan.bishop@pearson.com>
2015-08-23 18:59:06 -0500

    Remove installation of package duplicity

--- definitions/restore.rb
+++ definitions/restore.rb
@@ -24,8 +24,6 @@ define :duplicity_restore, :verbosity => 3,
   duplicity_cmd << " #{params[:local_path]}"
   duplicity_cmd << " --time #{params[:age]}"
 
-  package "duplicity"
-  
   execute "Restore #{params[:restore_item]} to #{params[:local_path]}" do
     action :run
     command duplicity_cmd
