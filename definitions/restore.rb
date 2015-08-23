9254c752e2d18e5a118d4ecf4f4657965da519ab
Bryan Bishop <bryan.bishop@pearson.com>
2015-08-23 14:21:26 -0500

    Add definition for duplicity restoring

---
+++ definitions/restore.rb
@@ -0,0 +1,35 @@
+define :duplicity_restore, :verbosity => 3,
+       :restore_item => nil,
+       :remote_path => nil,
+       :local_path => "restore",
+       :age => "now",
+       :restore_as_user => node['current_user'],
+       :duplicity_options => [],
+       :remote_path_password => nil,
+       :encryption_password => nil do
+
+  directory params[:local_path] do
+    recursive true
+  end
+
+  duplicity_cmd = "FTP_PASSWORD=#{params[:remote_path_password]}" if
+    params[:remote_path_password]
+  duplicity_cmd = " PASSPHRASE=#{params[:encryption_password]}" if
+    params[:encryption_password]]
+  duplicity_cmd = " duplicity"
+  params[:duplicity_options].each { |opt| duplicity_cmd << " #{opt}" }
+  duplicity_cmd << " --verbosity #{params[:verbosity]}"
+  duplicity_cmd << " --file-to-restore #{params[:restore_item]}"
+  duplicity_cmd << " #{remote_path}"
+  duplicity_cmd << " #{local_path}"
+  duplicity_cmd << " --time #{params[:age]}"
+  
+  execute "Restore #{params[:restore_item]} to #{params[:local_path]}" do
+    action :run
+    command duplicity_cmd
+    cwd ::Dir.home(params[:restore_as_user])
+    user params[:restore_as_user]
+    only_if ::Dir.exist?(params[:local_path])
+    only_if `which duplicity`
+  end
+end
---
+++ recipes/restore.rb
@@ -0,0 +1,16 @@
+passwd = ::File.readlines(::File.join(::Dir.home(node['current_user']), '.box'))[1]
+
+duplicity_restore ".apikeys" do
+  remote_item ".apikeys"
+  local_path File.join(::Dir.home(node["current_user"]), "restore")
+  remote_path "par2+webdavs://kc0.rjx@gmail.com@dav.box.com/dav/duplicity/"
+  remote_path_password passwd
+  encryption_password passwd
+  age "now"
+  restore_as_user node['current_user']
+  duplicity_options [
+    "--tempdir #{ENV['TMPDIR']}",
+    "--no-print-statistics",
+    "--extra-clean"
+  ]
+end
