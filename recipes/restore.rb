a17b3d6ec0189aa2afbc628b048d5a6713da73be
Bryan Bishop <bryan.bishop@pearson.com>
2015-08-23 19:14:56 -0500

    Take out password env variables, they're not working

--- recipes/restore.rb
+++ recipes/restore.rb
@@ -4,8 +4,6 @@ duplicity_restore ".apikeys" do
   restore_item ".apikeys"
   local_path File.join(::Dir.home(node["current_user"]), "restore")
   remote_path "par2+webdavs://kc0.rjx@gmail.com@dav.box.com/dav/duplicity/"
-  remote_path_password passwd
-  encryption_password passwd
   age "now"
   restore_as_user node['current_user']
   duplicity_options [
